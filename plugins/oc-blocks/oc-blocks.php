1b7cf75e7dc42fd5a12a0b2d85c98f13920f84c3
originalconcepts <george@originalconcepts.co.il>
2026-09-16 05:32:11 +0300

    v0.3.175

--- plugins/oc-blocks/oc-blocks.php
+++ plugins/oc-blocks/oc-blocks.php
@@ -20,7 +20,7 @@ declare( strict_types = 1 );
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'OC_BLOCKS_VERSION', '0.3.174' );
+define( 'OC_BLOCKS_VERSION', '0.3.175' );
 define( 'OC_BLOCKS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'OC_BLOCKS_URI', plugin_dir_url( __FILE__ ) );
 
